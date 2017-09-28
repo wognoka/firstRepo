a48267c977871c4de5455b96294dcfc14cce8d0c
ST8 <st8@ST8-iMac.local>
2017-09-28 11:12:32 +0300

    I added show button

--- KugimProject1/ViewController.swift
+++ KugimProject1/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func changeUILabelButton() {
+        label.text = "UI Label Changed"
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
